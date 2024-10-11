2e88fec934f46105f0d3003fb3739d0e14d12927
piyathidaK145 <piyathidak65@nu.ac.th>
2024-10-11 16:27:42 +0700

    Merge branch 'main' of https://github.com/PiyathidaK145/account

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,8 +1,11 @@
-import 'package:account/main.dart';
-import 'package:account/models/transactions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:account/models/transactions.dart';
 import 'package:account/provider/transaction_provider.dart';
+import 'package:account/main.dart';
+import 'package:intl/intl.dart'; // Import intl for date formatting
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 class FormScreen extends StatefulWidget {
   const FormScreen({super.key});
@@ -15,8 +18,12 @@ class _FormScreenState extends State<FormScreen> {
   final formKey = GlobalKey<FormState>();
   final titleController = TextEditingController();
   final amountController = TextEditingController();
-  
-  DateTime? selectedDate; // Variable to store the selected date
+  final contactController = TextEditingController(); // For contact information
+  final descriptionController = TextEditingController(); // For description
+  final fieldController = TextEditingController(); // For scientific field
+
+  DateTime? selectedDate;
+  File? selectedImage;
 
   // Method to show date picker
   Future<void> _selectDate(BuildContext context) async {
@@ -26,10 +33,22 @@ class _FormScreenState extends State<FormScreen> {
       firstDate: DateTime(2000),
       lastDate: DateTime(2101),
     );
-    if (picked != null && picked != selectedDate)
+    if (picked != null && picked != selectedDate) {
       setState(() {
-        selectedDate = picked; // Update selected date
+        selectedDate = picked;
       });
+    }
+  }
+
+  // Method to pick image from gallery
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        selectedImage = File(pickedFile.path);
+      });
+    }
   }
 
   @override
@@ -40,173 +59,118 @@ class _FormScreenState extends State<FormScreen> {
       ),
       body: Form(
         key: formKey,
-        child: Column(
-          children: [
-            // Display the selected date
-            ListTile(
-              title: Text(
-                selectedDate == null
-                    ? 'เลือกวันที่'
-                    : 'วันที่: ${selectedDate!.toLocal()}'.split(' ')[0], // Display date in 'yyyy-mm-dd' format
-              ),
-              trailing: const Icon(Icons.calendar_today),
-              onTap: () => _selectDate(context), // Show date picker on tap
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'ชื่อรายการ',
-              ),
-              autofocus: false,
-              controller: titleController,
-              validator: (String? str) {
-                if (str!.isEmpty) {
-                  return 'กรุณากรอกข้อมูล';
-                }
-              },
-            ),
-            TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'จำนวนเงิน',
-                  ),
-                  keyboardType: TextInputType.number,
-                  controller: amountController,
-                  validator: (String? input) {
-                    try {
-                      double amount = double.parse(input!);
-                      if (amount < 0) {
-                        return 'กรุณากรอกข้อมูลมากกว่า 0';
-                      }
-                    } catch (e) {
-                      return 'กรุณากรอกข้อมูลเป็นตัวเลข';
-                    }
-                  },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Display the selected date
+              ListTile(
+                title: Text(
+                  selectedDate == null
+                      ? 'เลือกวันที่'
+                      : 'วันที่: ${DateFormat('dd/MM/yyyy').format(selectedDate!)}', // Format date as 'dd/MM/yyyy'
                 ),
-            TextButton(
-              child: const Text('บันทึก'),
-              onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  // Create transaction data object with the selected date
-                  var statement = Transactions(
-                    keyID: null,
-                    title: titleController.text,
-                    amount: double.parse(amountController.text),
-                    date: selectedDate ?? DateTime.now(), // Use selected date or current date
-                  );
-
-                  // Add transaction data object to provider
-                  var provider = Provider.of<TransactionProvider>(context, listen: false);
-                  provider.addTransaction(statement);
-
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      fullscreenDialog: true,
-                      builder: (context) {
-                        return MyHomePage();
-                      },
+                trailing: const Icon(Icons.calendar_today),
+                onTap: () => _selectDate(context),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'ชื่อรายการ'),
+                controller: titleController,
+                validator: (String? str) {
+                  if (str!.isEmpty) {
+                    return 'กรุณากรอกข้อมูล';
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'จำนวนเงิน'),
+                keyboardType: TextInputType.number,
+                controller: amountController,
+                validator: (String? input) {
+                  try {
+                    double amount = double.parse(input!);
+                    if (amount < 0) {
+                      return 'กรุณากรอกข้อมูลมากกว่า 0';
+                    }
+                  } catch (e) {
+                    return 'กรุณากรอกข้อมูลเป็นตัวเลข';
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'ช่องทางการติดต่อ'),
+                controller: contactController,
+                validator: (String? str) {
+                  if (str!.isEmpty) {
+                    return 'กรุณากรอกข้อมูลการติดต่อ';
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'รายละเอียด'),
+                controller: descriptionController,
+                validator: (String? str) {
+                  if (str!.isEmpty) {
+                    return 'กรุณากรอกข้อมูลรายละเอียด';
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'ศาสตร์ที่ใช้'),
+                controller: fieldController,
+                validator: (String? str) {
+                  if (str!.isEmpty) {
+                    return 'กรุณากรอกข้อมูลศาสตร์ที่ใช้';
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              // Image picker button
+              selectedImage == null
+                  ? TextButton.icon(
+                      icon: const Icon(Icons.image),
+                      label: const Text('อัพโหลดรูปภาพ'),
+                      onPressed: _pickImage,
+                    )
+                  : Image.file(
+                      selectedImage!,
+                      height: 150,
+                      width: 150,
                     ),
-                  );
-                
-                }
-              },
-            ),
-          ],
+              TextButton(
+                child: const Text('บันทึก'),
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    // Create transaction data object with selected data and image
+                    var statement = Transactions(
+                      keyID: null,
+                      title: titleController.text,
+                      amount: double.parse(amountController.text),
+                      date: selectedDate ?? DateTime.now(),
+                      contact: contactController.text,
+                      description: descriptionController.text,
+                      field: fieldController.text,
+                      image: selectedImage != null ? selectedImage!.path : null,
+                    );
+
+                    var provider = Provider.of<TransactionProvider>(context, listen: false);
+                    provider.addTransaction(statement);
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        fullscreenDialog: true,
+                        builder: (context) {
+                          return MyHomePage();
+                        },
+                      ),
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-/*import 'package:account/main.dart';
-import 'package:account/models/transactions.dart';
-
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:account/provider/transaction_provider.dart';
-
-class FormScreen extends StatefulWidget {
-
-
-  const FormScreen({super.key});
-
-  @override
-  State<FormScreen> createState() => _FormScreenState();
-}
-
-class _FormScreenState extends State<FormScreen> {
-  final formKey = GlobalKey<FormState>();
-
-  final titleController = TextEditingController();
-
-  final amountController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-  
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('แบบฟอร์มเพิ่มข้อมูล'),
-        ),
-        body: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'ชื่อรายการ',
-                  ),
-                  autofocus: false,
-                  controller: titleController,
-                  validator: (String? str) {
-                    if (str!.isEmpty) {
-                      return 'กรุณากรอกข้อมูล';
-                    }
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'จำนวนเงิน',
-                  ),
-                  keyboardType: TextInputType.number,
-                  controller: amountController,
-                  validator: (String? input) {
-                    try {
-                      double amount = double.parse(input!);
-                      if (amount < 0) {
-                        return 'กรุณากรอกข้อมูลมากกว่า 0';
-                      }
-                    } catch (e) {
-                      return 'กรุณากรอกข้อมูลเป็นตัวเลข';
-                    }
-                  },
-                ),
-                TextButton(
-                    child: const Text('บันทึก'),
-                    onPressed: () {
-                          if (formKey.currentState!.validate())
-                            {
-                              // create transaction data object
-                              var statement = Transactions(
-                                  keyID: null,
-                                  title: titleController.text,
-                                  amount: double.parse(amountController.text),
-                                  date: DateTime.now()
-                                  );
-                            
-                              // add transaction data object to provider
-                              var provider = Provider.of<TransactionProvider>(context, listen: false);
-                              
-                              provider.addTransaction(statement);
-
-                              Navigator.push(context, MaterialPageRoute(
-                                fullscreenDialog: true,
-                                builder: (context){
-                                  return MyHomePage();
-                                }
-                              ));
-                            }
-                        })
-              ],
-            )));
-  }
-}*/
